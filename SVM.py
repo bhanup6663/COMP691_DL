34121b0b8a347866e7b8cc495d51cbda510feb7a
Niloofar Ak <113780591+NiloofarAk@users.noreply.github.com>
2024-03-16 18:08:56 -0400

    Update SVM.py

--- SVM.py
+++ SVM.py
@@ -7,10 +7,8 @@ from sklearn.metrics import classification_report
 import numpy as np
 import random
 
-# Set device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# CIFAR-10 data loading
 transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
@@ -22,11 +20,9 @@ trainloader = torch.utils.data.DataLoader(trainset, batch_size=len(trainset), sh
 testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=len(testset), shuffle=False)
 
-# Extract and preprocess data
 def extract_data(loader):
     for data in loader:
         images, labels = data
-        # Flatten images and move data to numpy
         images = images.numpy().reshape(images.size(0), -1)
         labels = labels.numpy()
         return images, labels
@@ -48,10 +44,8 @@ y_train = y_train.ravel()
 x_test, _, y_test, _ = train_test_split(x_test, y_test, test_size=10, stratify=y_test)
 y_test = y_test.ravel()
 
-# Simulate training "epochs"
 svm_model = SVC(kernel='linear')
 svm_model.fit(x_train, y_train)
     
-# Predict and evaluate
 predictions = svm_model.predict(x_test)
 print(classification_report(y_test, predictions))
